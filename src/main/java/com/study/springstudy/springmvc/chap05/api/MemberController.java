5f062c1b20ea25e2c88146657a6438a372fae5cd
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-05-31 10:52:18 +0900

    login session

--- src/main/java/com/study/springstudy/springmvc/chap05/api/MemberController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/MemberController.java
@@ -4,7 +4,6 @@ import com.study.springstudy.springmvc.chap05.dto.request.LoginDto;
 import com.study.springstudy.springmvc.chap05.dto.request.SignUpDto;
 import com.study.springstudy.springmvc.chap05.service.LoginResult;
 import com.study.springstudy.springmvc.chap05.service.MemberService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,46 +14,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import static com.study.springstudy.springmvc.chap05.service.LoginResult.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-@RequiredArgsConstructor
 @Controller
 @RequestMapping("/members")
 @Slf4j
+@RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
     // 회원가입 양식 열기
     @GetMapping("/sign-up")
-//    @ResponseBody
-    // url이 jsp경로와 같으면 void로 설정해도 알아서 위에꺼 가져와서 열기 가능
-    public void signUp(){
+    public void signUp() {
         log.info("/members/sign-up GET : forwarding to sign-up.jsp");
-        //return "members/sign-up";
+        // return "members/sign-up";
     }
 
     // 회원가입 요청 처리
     @PostMapping("/sign-up")
-    public String signUp(SignUpDto dto){
+    public String signUp(@Validated SignUpDto dto) {
         log.info("/members/sign-up POST ");
         log.debug("parameter: {}", dto);
 
         boolean flag = memberService.join(dto);
 
-        return flag? "redirect:/members/sign-in" : "redirect:/members/sign-up";
+        return flag ? "redirect:/members/sign-in" : "redirect:/members/sign-up";
     }
 
-    // 아이디, 이메일 중보ㅓㅁ사 비동기 요청처리
+    // 아이디, 이메일 중복검사 비동기 요청 처리
     @GetMapping("/check")
     @ResponseBody
-    public ResponseEntity<?> check(String type, String keyword){
+    public ResponseEntity<?> check(String type, String keyword) {
         boolean flag = memberService.checkIdentifier(type, keyword);
-        log.info("{} 중복체크 결과? {}" , type, flag);
-        return ResponseEntity.ok().body(flag);
+        log.debug("{} 중복체크 결과? {}", type, flag);
+        return ResponseEntity
+                .ok()
+                .body(flag);
     }
 
     // 로그인 양식 열기
@@ -65,23 +64,32 @@ public class MemberController {
 
     // 로그인 요청 처리
     @PostMapping("/sign-in")
-    public String signIn(LoginDto dto, RedirectAttributes ra){
-        log.info("members/sign-in POST");
+    public String signIn(LoginDto dto,
+                         RedirectAttributes ra,
+                         HttpServletRequest request) {
+        log.info("/members/sign-in POST");
         log.debug("parameter: {}", dto);
 
-        LoginResult result = memberService.authenticate(dto);
+        // 세션 얻기
+        HttpSession session = request.getSession();
+
+        LoginResult result = memberService.authenticate(dto, session);
 
         // 로그인 검증 결과를 JSP에게 보내기
-        // redirect시에 Redirect된 페이지에 데이터를 보낼 때는
+        // Redirect시에 Redirect된 페이지에 데이터를 보낼 때는
         // Model객체를 사용할 수 없음
-        // dhosigkaus Model객체는 request객체를 사용하는데 해당 객체는
-        // 한번의 요청이 끝나면 메모리에서 제거된다.
-        // 그러나 redirect는 요청이 2번 발생하므로 다른 request 객체를 jsp가 사용하게 됨
+        // 왜냐면 Model객체는 request객체를 사용하는데 해당 객체는
+        // 한번의 요청이 끝나면 메모리에서 제거된다. 그러나 redirect는
+        // 요청이 2번 발생하므로 다른 request객체를 jsp가 사용하게 됨
+
+//        model.addAttribute("result", result); // (X)
         ra.addFlashAttribute("result", result);
 
-        if(result == SUCCESS){
+        if (result == LoginResult.SUCCESS) {
             return "redirect:/index"; // 로그인 성공시
         }
+
         return "redirect:/members/sign-in";
     }
+
 }
--- src/main/java/com/study/springstudy/springmvc/chap05/service/MemberService.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/service/MemberService.java
@@ -2,12 +2,16 @@ package com.study.springstudy.springmvc.chap05.service;
 
 import com.study.springstudy.springmvc.chap05.dto.request.LoginDto;
 import com.study.springstudy.springmvc.chap05.dto.request.SignUpDto;
+import com.study.springstudy.springmvc.chap05.dto.response.LoginUserInfoDto;
 import com.study.springstudy.springmvc.chap05.entity.Member;
 import com.study.springstudy.springmvc.chap05.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 
 import static com.study.springstudy.springmvc.chap05.service.LoginResult.*;
 
@@ -31,15 +35,16 @@ public class MemberService {
         return memberMapper.save(member);
     }
 
-    // 로그인 검증처리
-    public LoginResult authenticate(LoginDto dto){
+
+    // 로그인 검증 처리
+    public LoginResult authenticate(LoginDto dto, HttpSession session) {
 
         // 회원가입 여부 확인
         String account = dto.getAccount();
         Member foundMember = memberMapper.findOne(account);
 
-        if(foundMember == null){
-            log.info("{} -  회원가입이 필요합니다.", account);
+        if (foundMember == null) {
+            log.info("{} - 회원가입이 필요합니다.", account);
             return NO_ACC;
         }
 
@@ -48,20 +53,27 @@ public class MemberService {
         String originPassword = foundMember.getPassword(); // 데이터베이스에 저장된 비번
 
         // PasswordEncoder에서는 암호화된 비번을 내부적으로 비교해주는 기능을 제공
-        if(!encoder.matches(inputPassword, originPassword)){
-            log.info("비밀번호가 일치하지 않습니다");
+        if (!encoder.matches(inputPassword, originPassword)) {
+            log.info("비밀번호가 일치하지 않습니다.");
             return NO_PW;
         }
 
         log.info("{}님 로그인 성공", foundMember.getName());
-        return SUCCESS;
+        session.setAttribute("loginUserName", foundMember.getName());
 
+        // 세션의 수명 : 설정된 시간 OR 브라우저를 닫기 전까지
+        int maxInactiveInterval = session.getMaxInactiveInterval();
+        session.setMaxInactiveInterval(60*60); // 세션 수명 1시간 설정 90일
+        log.debug("session Time: {}", maxInactiveInterval);
+
+        session.setAttribute("login", new LoginUserInfoDto(foundMember));
+        return SUCCESS;
     }
+
 
     // 아이디, 이메일 중복검사
-    public boolean checkIdentifier(String type, String keyword){
+    public boolean checkIdentifier(String type, String keyword) {
         return memberMapper.existsById(type, keyword);
     }
-
 
 }
