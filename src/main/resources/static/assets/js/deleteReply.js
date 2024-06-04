48479605d6d4e32151871f598d444201c859261b
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-04 14:30:26 +0900

    reply Delete and modify auth

--- src/main/resources/static/assets/js/deleteReply.js
+++ src/main/resources/static/assets/js/deleteReply.js
@@ -1,20 +1,10 @@
 import { BASE_URL } from "./reply.js";
 import { fetchInfScrollReplies, removeInfiniteScroll } from "./getReply.js";
-
+import {callApi} from "./api.js";
 // 댓글 삭제 비동기 요청 처리 함수
 const fetchDeleteReply = async (rno) => {
 
-  const res = await fetch(`${BASE_URL}/${rno}`, {
-    method: 'DELETE'
-  });
-
-  if (res.status !== 200) {
-    alert('삭제에 실패했습니다!');
-    return;
-  }
-
-  window.scrollTo(0, 0); // 삭제 후 페이지 상단으로 이동
-  await fetchInfScrollReplies();
+  await callApi(`${BASE_URL}/${rno}`, 'DELETE');
 };
 
 // 댓글 삭제 처리 이벤트 등록 함수
